b632b75a192bbf67420ad509d57e14d5d2ff5594
Ragnar Laud <ragnar.laud@hotmail.com>
2025-01-01 23:11:21 +0200

    Update environment
    Add KV_HOST and KV_PORT to EnvironmentSchema
    Add NUM_WORKERS

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,10 @@ import "dotenv/config";
 
 export const EnvironmentSchema = z.object({
     BROWSER_PATH: z.string(),
+    KV_HOST: z.string(),
+    KV_PORT: z.number({ coerce: true }),
+
+    NUM_WORKERS: z.number({ coerce: true }).optional().default(1),
 });
 
 export const env = EnvironmentSchema.parse(process.env);
